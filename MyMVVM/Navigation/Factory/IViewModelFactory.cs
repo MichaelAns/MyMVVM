8c358b9fc68e46b56cab594e54322cff1de1e865
Михаил Щукин <mi035ms@gmail.com>
2022-12-02 14:18:00 +0300

    Создание IViewModelFactory

---
+++ MyMVVM/Navigation/Factory/IViewModelFactory.cs
@@ -0,0 +1,17 @@
+﻿using MyMVVM.ViewModelBase;
+
+namespace MyMVVM.Navigation.Factory
+{
+    /// <summary>
+    /// The factory for creating specific ViewModel 
+    /// </summary>
+    /// <typeparam name="T">T must be ViewModel</typeparam>
+    public interface IViewModelFactory<T> where T : ViewModel
+    {
+        /// <summary>
+        /// Method for creating specific ViewModel
+        /// </summary>
+        /// <returns>Specific ViewModel</returns>
+        public T CreateViewModel();
+    }
+}
