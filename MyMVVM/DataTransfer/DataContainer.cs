266b9f121fe9a04eb885eb8187303af765304b32
Михаил Щукин <mi035ms@gmail.com>
2022-12-05 02:06:13 +0300

    Изменение передачи данных

--- MyMVVM/DataTransfer/DataContainer.cs
+++ MyMVVM/DataTransfer/DataContainer.cs
@@ -2,8 +2,13 @@
 {
     public class DataContainer
     {
-        private DataContainer() { }
-        public DataMessage DataMessage { get; set; }
+        private DataContainer()
+        {
+            if (_needToDelete)
+            {
+                _dataMessage = null;
+            }
+        }
 
         private static DataContainer instance;
         public static DataContainer GetInstance()
@@ -14,6 +19,19 @@
                 return instance;
             }
             return instance;
+        }
+
+        private bool _needToDelete = false;
+        private DataMessage _dataMessage;
+        public DataMessage GetDataMessage()
+        {
+            _needToDelete = true;
+            return _dataMessage;
+        }
+        public void SendDataMessage(DataMessage dataMessage)
+        {
+            _needToDelete = false;
+            _dataMessage = dataMessage;
         }
 
     }
--- MyMVVM/DataTransfer/DataMessage.cs
+++ MyMVVM/DataTransfer/DataMessage.cs
@@ -1,18 +1,4 @@
 ﻿namespace MyMVVM.DataTransfer
 {
-    public class DataMessage
-    {
-        public DataMessage(DataMessageArgs args)
-        {
-            _args = args;
-        }
-
-        private readonly DataMessageArgs _args;
-        public DataMessageArgs Args
-        {
-            get => _args;   
-        }
-
-        
-    }
+    public abstract class DataMessage { }
 }
