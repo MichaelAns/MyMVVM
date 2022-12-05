bff5f0f9f59bc666c108f81b2e4fa34beeae320a
Михаил Щукин <mi035ms@gmail.com>
2022-12-06 01:09:31 +0300

    Изменения ренавигатора

---
+++ MyMVVM/Navigation/Navigators/ViewModelDelegatingRenavigator.cs
@@ -0,0 +1,22 @@
+﻿using MyMVVM.Navigation.Factory;
+using MyMVVM.ViewModelBase;
+
+namespace MyMVVM.Navigation.Navigators
+{
+    public class ViewModelDelegatingRenavigator<TViewModel> : IRenavigator where TViewModel:ViewModel
+    {
+        private readonly INavigator _navigator;
+        private readonly CreateViewModel<TViewModel> _createViewModel;
+
+        public ViewModelDelegatingRenavigator(INavigator navigator, CreateViewModel<TViewModel> createViewModel)
+        {
+            _navigator = navigator;
+            _createViewModel = createViewModel;
+        }
+
+        public void Renavigate()
+        {
+            _navigator.CurrentViewModel = _createViewModel();
+        }
+    }
+}
